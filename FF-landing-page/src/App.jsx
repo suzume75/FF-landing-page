8c910e464f23fafbe87e50a905eb0024a694f8b5
suzume75 <kl6130422@gmail.com>
2025-07-05 14:19:56 +0100

    done with the hero section

--- FF-landing-page/src/App.jsx
+++ FF-landing-page/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import React from 'react'
 import Navbar from './navbar.jsx'
+import Hero from './hero.jsx'
 
 function App() {
 
   return (
     <>
     <Navbar />
+    <Hero />
     </>
   )
 }
