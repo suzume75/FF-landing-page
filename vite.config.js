f15aca6d239825dba604561e061a18ec426ac4be
suzume75 <kl6130422@gmail.com>
2025-07-09 22:43:54 +0100

    small fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/FF-landing-page/', 
+  base: '/FF-landing-page/',
 })
